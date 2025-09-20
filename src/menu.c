6d11a8fddd31b51be759bc9a7e4e349d9d70a56e
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-09-20 00:25:24 -0300

    integração com os programas externos.
    Apenas uma simples chamadas, e também aborda as opções de cada, que são bem pequenas comparada a este.

--- src/menu.c
+++ src/menu.c
@@ -2,6 +2,9 @@
 #include "menu.h"
 // Biblioteca padrão do C:
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
 // Outros módulos do projeto:
 #include "filtro.h"
 #include "variaveis_de_ambiente.h"
@@ -49,17 +52,56 @@ static void folha_de_ajuda(void) {
       "\t\t-i, --info-projeto <RAIZ>\n\t\t\b informação processada de todos arquivos de tal projeto.\n" 
       "\t\t-L, --repositorio-links \n\t\t\b os linques dos repostiórios de "
       "\n\t\tlinques de programas feitos, e se os linques ali, ainda são válidos"
+      "\n\t\t-t, --todos-comandos \n\t\t\b Todo histórico de comandos já "
+      "\n\t\tdigitado, e sua respectiva frequência."
+      "\n\t\t-M, --comandos-mais-relevantes \n\t\t\b Apenas os comandos com "
+      "\n\t\talguma relevância numérica."
+      "\n\t\t-e, --rust-antigo-cache \n\t\t\b A o gerenciador de pacotes  "
+      "\n\t\texternos faz um bocado de downloads, este aqui lista e organiza-os."
       // Quebra-de-linha no final.
       "\n\n",
       RECUO, PROG, RECUO, PROG, PROG, RECUO
    );
 }
 
+#ifdef __debug__
+static void visualiza_argumentos(char* args[], int quantia) {
+   const char* TAB = "\t\b\b\b";
+
+   printf("Atual diretório: %s\n", getcwd(NULL, 255));
+   printf("Quantidade: %d\n", quantia);
+
+   if (quantia > 0) {
+      for (int n = 1; n <= quantia; n++)
+         printf("%s==> '%s'\n", TAB, args[n - 1]);
+   }
+}
+#endif
+
+
 extern void menu_interface_do_programa(char* args[], int total)
 {
-   const char* const CURTAS = "hi::P::L::";
+   const char* const CURTAS = "hi::P::L::M::t::e::";
    int result = getopt(total, args, CURTAS);
    const int Falhou = -1;
+   const int MAX = 2 * UCHAR_MAX;
+
+   #ifdef __debug__
+   const char* programas[] = {
+      "/home/alice/Documents/códigos/c-language/codigo-info/bin/programs/cmd-frequencia-debug",
+      "/home/alice/Documents/códigos/c-language/codigo-info/bin/programs/pacotes-externos-debug",
+   };
+   #else
+   const char* programas[] = {
+      "/home/alice/Documents/códigos/c-language/codigo-info/bin/programs/cmd-frequencia",
+      "/home/alice/Documents/códigos/c-language/codigo-info/bin/programs/pacotes-externos",
+   };
+   #endif
+   int exitcode;
+
+   #ifdef __debug__
+   visualiza_argumentos(args, total);
+   #endif
 
    while (result != Falhou) {
       char opcao = (char)result;
@@ -77,11 +119,34 @@ extern void menu_interface_do_programa(char* args[], int total)
          DirInfo agregado = processa_projeto(caminho);
          visualiza_diretorio_info(&agregado);
 
-      } else if (opcao == 'L') {
-         putchar('\n');
+      } else if (opcao == 'L') 
          info_sobre_repositorio_de_linques();
-         putchar('\n');
+      else if (opcao == 't' || opcao == 'M') {
+         exitcode = execl(
+            programas[0], "cmd-frquencia", 
+            args[1], (char*)NULL
+         );
+
+         if (exitcode == -1) {
+            puts("O programa falhou na execução.");
+            perror(strerror(errno));
+
+         } else
+            puts("O programa executou normalmente.");
+      } else if (opcao == 'e') {
+         exitcode = execl(
+            programas[1], "pacotes-externos", 
+            NULL, (char*)NULL
+         );
+
+         if (exitcode == -1) {
+            puts("O programa falhou na execução.");
+            perror(strerror(errno));
+
+         } else
+            puts("O programa executou normalmente.");
       }
+
 
       #ifdef __debug__
       variaveis_externas();
